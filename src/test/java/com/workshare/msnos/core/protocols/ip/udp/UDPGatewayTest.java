6791943fa6ee123c42fe2b75c2f08e81f531133d
RhysWorkshare <rhys.davies@workshare.com>
2014-04-03 15:03:07 +0100

    Update to add IDEA files

--- src/test/java/com/workshare/msnos/core/protocols/ip/udp/UDPGatewayTest.java
+++ src/test/java/com/workshare/msnos/core/protocols/ip/udp/UDPGatewayTest.java
@@ -1,56 +1,44 @@
 package com.workshare.msnos.core.protocols.ip.udp;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.MulticastSocket;
-import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Executor;
-
+import com.workshare.msnos.core.Gateway.Listener;
+import com.workshare.msnos.core.Message;
+import com.workshare.msnos.core.protocols.ip.MulticastSocketFactory;
+import com.workshare.msnos.soup.json.Json;
+import com.workshare.msnos.soup.threading.Multicaster;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
-import com.workshare.msnos.core.Gateway.Listener;
-import com.workshare.msnos.core.Message;
-import com.workshare.msnos.core.protocols.ip.MulticastSocketFactory;
-import com.workshare.msnos.core.protocols.ip.udp.UDPGateway;
-import com.workshare.msnos.soup.json.Json;
-import com.workshare.msnos.soup.threading.Multicaster;
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class UDPGatewayTest {
 
     private UDPGateway gate;
-	private UDPServer server;
+    private UDPServer server;
     private MulticastSocket socket;
     private MulticastSocketFactory sockets;
     private List<Message> messages;
-    
+
     @Before
     public void setup() throws Exception {
-    	messages = new ArrayList<Message>();
+        messages = new ArrayList<Message>();
 
-    	server = mock(UDPServer.class);
-    	socket = mock(MulticastSocket.class);
+        server = mock(UDPServer.class);
+        socket = mock(MulticastSocket.class);
         sockets = mock(MulticastSocketFactory.class);
         when(sockets.create()).thenReturn(socket);
     }
-    
+
     @Test
     public void shouldOpenTheSocket() throws Exception {
-        System.setProperty(UDPGateway.SYSP_PORT_NUM,  "2727");
+        System.setProperty(UDPGateway.SYSP_PORT_NUM, "2727");
 
         gate();
 
@@ -60,33 +48,33 @@ public class UDPGatewayTest {
 
     @Test
     public void shouldJoinTheUDPGroup() throws Exception {
-        System.setProperty(UDPGateway.SYSP_UDP_GROUP,  "230.31.32.33");
-        
+        System.setProperty(UDPGateway.SYSP_UDP_GROUP, "230.31.32.33");
+
         ArgumentCaptor<InetAddress> captor = ArgumentCaptor.forClass(InetAddress.class);
         gate();
         verify(socket).joinGroup(captor.capture());
 
         assertEquals("230.31.32.33", captor.getValue().getHostAddress());
     }
-    
+
     @Test
     public void shouldUseNextSocketPort() throws Exception {
-        System.setProperty(UDPGateway.SYSP_PORT_NUM,  "2727");
-        System.setProperty(UDPGateway.SYSP_PORT_WIDTH,  "2");
+        System.setProperty(UDPGateway.SYSP_PORT_NUM, "2727");
+        System.setProperty(UDPGateway.SYSP_PORT_WIDTH, "2");
         doThrow(new SocketException("boom!")).when(socket).bind(new InetSocketAddress(2727));
 
         gate();
         verify(socket).bind(new InetSocketAddress(2728));
     }
-    
+
     @Test(expected = IOException.class)
     public void shouldBlowUpIfNoPortAvailable() throws Exception {
-        System.setProperty(UDPGateway.SYSP_PORT_NUM,  "2727");
-        System.setProperty(UDPGateway.SYSP_PORT_WIDTH,  "1");
+        System.setProperty(UDPGateway.SYSP_PORT_NUM, "2727");
+        System.setProperty(UDPGateway.SYSP_PORT_WIDTH, "1");
         doThrow(new SocketException("boom!")).when(socket).bind(new InetSocketAddress(2727));
 
-		gate();
-	}
+        gate();
+    }
 
     @Test
     public void shouldSendAMessageTroughTheSocket() throws Exception {
@@ -99,18 +87,18 @@ public class UDPGatewayTest {
 
     @Test
     public void shouldSendAMessageToEachPort() throws Exception {
-        System.setProperty(UDPGateway.SYSP_UDP_GROUP,  "230.31.32.33");
-        System.setProperty(UDPGateway.SYSP_PORT_NUM,  "2727");
-        System.setProperty(UDPGateway.SYSP_PORT_WIDTH,  "3");
-        
+        System.setProperty(UDPGateway.SYSP_UDP_GROUP, "230.31.32.33");
+        System.setProperty(UDPGateway.SYSP_PORT_NUM, "2727");
+        System.setProperty(UDPGateway.SYSP_PORT_WIDTH, "3");
+
         Message message = Utils.newSampleMessage();
         gate().send(message);
 
         List<DatagramPacket> packets = getSentPackets();
         assertEquals(3, packets.size());
-        
+
         int port = 2727;
-        for (DatagramPacket packet: packets) {
+        for (DatagramPacket packet : packets) {
             assertPacketValid(message, packet);
             assertEquals(port++, packet.getPort());
             assertEquals(InetAddress.getByName("230.31.32.33"), packet.getAddress());
@@ -125,26 +113,42 @@ public class UDPGatewayTest {
 
     @Test
     public void shouldInvokeListenerOnMessageReceived() throws Exception {
-        gate().addListener(new Listener(){
-			@Override
-			public void onMessage(Message message) {
-				messages.add(message);
-			}});
-        
+        gate().addListener(new Listener() {
+            @Override
+            public void onMessage(Message message) {
+                messages.add(message);
+            }
+        });
+
         ArgumentCaptor<Listener> serverListener = ArgumentCaptor.forClass(Listener.class);
         verify(server).addListener(serverListener.capture());
 
         Message message = Utils.newSampleMessage();
         serverListener.getValue().onMessage(message);
-        
+
         assertEquals(1, messages.size());
         assertEquals(message, messages.get(0));
     }
 
-    // public void shouldInvokeListenerOnMessagesAddressedToMe() 
-    // public void shouldInvokeListenerOnMessagesAddressedToMyCloud() 
-    // public void shouldNOTInvokeListenerOnMessagesAddressedToSomeoneElse() 
-    
+    @Test
+    public void shouldInvokeListenerOnMessagesAddressedToMe() throws IOException {
+        gate().addListener(new Listener() {
+            @Override
+            public void onMessage(Message message) {
+                messages.add(message);
+            }
+        });
+
+        ArgumentCaptor<Listener> serverListener = ArgumentCaptor.forClass(Listener.class);
+        verify(server).addListener(serverListener.capture());
+
+        Message someoneElse = Utils.newSampleMessageToOther();
+        for (Message msg : messages) assertNotSame(someoneElse, msg);
+    }
+
+    //TODO public void shouldInvokeListenerOnMessagesAddressedToMyCloud()
+    //TODO public void shouldNOTInvokeListenerOnMessagesAddressedToSomeoneElse()
+
     private void assertPacketValid(Message message, final DatagramPacket packet) {
         byte[] actuals = packet.getData();
         byte[] expecteds = Json.toBytes(message);
@@ -157,24 +161,26 @@ public class UDPGatewayTest {
         return packetCaptor.getAllValues();
     }
 
-	private UDPGateway gate() throws IOException {
-		if (gate == null)
-			gate= new UDPGateway(sockets, server, synchronousMulticaster());
+    private UDPGateway gate() throws IOException {
+        if (gate == null)
+            gate = new UDPGateway(sockets, server, synchronousMulticaster());
 
-		return gate;
-	}
+        return gate;
+    }
 
-	private Multicaster<Listener, Message> synchronousMulticaster() {
-		Executor executor = new Executor() {
-			@Override
-			public void execute(Runnable task) {
-				task.run();
-			}};
+    private Multicaster<Listener, Message> synchronousMulticaster() {
+        Executor executor = new Executor() {
+            @Override
+            public void execute(Runnable task) {
+                task.run();
+            }
+        };
 
-		return new Multicaster<Listener, Message>(executor){
-			@Override
-			protected void dispatch(Listener listener, Message message) {
-				listener.onMessage(message);
-			}};
-	}
+        return new Multicaster<Listener, Message>(executor) {
+            @Override
+            protected void dispatch(Listener listener, Message message) {
+                listener.onMessage(message);
+            }
+        };
+    }
 }
--- src/test/java/com/workshare/msnos/core/protocols/ip/udp/Utils.java
+++ src/test/java/com/workshare/msnos/core/protocols/ip/udp/Utils.java
@@ -1,9 +1,9 @@
 package com.workshare.msnos.core.protocols.ip.udp;
 
-import java.util.UUID;
-
 import com.workshare.msnos.core.Iden;
 import com.workshare.msnos.core.Message;
+
+import java.util.UUID;
 
 // an horrible static class to host utility methods
 // to share across tests
@@ -13,6 +13,14 @@ import com.workshare.msnos.core.Message;
 public class Utils {
     public static Message newSampleMessage() {
         final UUID uuid = new UUID(123, 456);
+        final Iden src = new Iden(Iden.Type.AGT, uuid);
+        final Iden dst = new Iden(Iden.Type.CLD, uuid);
+        final Message message = new Message(Message.Type.APP, src, dst, "sigval", 1, false, null);
+        return message;
+    }
+
+    public static Message newSampleMessageToOther() {
+        final UUID uuid = new UUID(123, 101112);
         final Iden src = new Iden(Iden.Type.AGT, uuid);
         final Iden dst = new Iden(Iden.Type.CLD, uuid);
         final Message message = new Message(Message.Type.APP, src, dst, "sigval", 1, false, null);
